6969d40df361b895c636235525c42119cfe84e8d
Daft <voglauer.daniel@gmail.com>
2026-02-02 20:47:01 +0100

    add IterationBenchmark.iterateByList

--- src/jmh/java/com/daftvoid/IterationBenchmark.java
+++ src/jmh/java/com/daftvoid/IterationBenchmark.java
@@ -2,7 +2,9 @@ package com.daftvoid;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -10,11 +12,22 @@ import java.util.stream.IntStream;
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Warmup(iterations = 3, time = 1)
 @Measurement(iterations = 3, time = 1)
-@Fork(1)
+@Fork(2)
 @State(Scope.Thread)
 public class IterationBenchmark {
     private int[] arr = new int[1000];
+    private List<Integer> list;
     private static volatile int sink;
+
+    @Setup
+    public void setup() {
+        list = new ArrayList<>(arr.length);
+
+        for (int x : arr) {
+            list.add(x);
+        }
+    }
+
 
     @Benchmark
     public void iterateByFor() {
@@ -26,5 +39,10 @@ public class IterationBenchmark {
     @Benchmark
     public void iterateByStream() {
         IntStream.of(arr).forEach(i -> sink = i);
+    }
+
+    @Benchmark
+    public void iterateByList() {
+        list.forEach(i -> sink = i);
     }
 }
